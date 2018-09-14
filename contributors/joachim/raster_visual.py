317fc6e37e5da71bccba09394889f19efcc74ac8
Joachim Meyer <j.meyer@utah.edu>
2018-09-13 19:36:41 -0700

    Moving files and data for presentation.
    Move unused files to contributor folder and add Michelle's
    presnetation to notebooks.

---
+++ contributors/joachim/raster_visual.py
@@ -0,0 +1,132 @@
+import glob
+import os
+
+import gdal, gdalnumeric
+import matplotlib.pyplot as plt
+import numpy as np
+import xarray as xr
+import statsmodels.api as sm
+
+from pygeotools.lib import iolib, warplib, geolib, timelib
+from scipy.constants import alpha
+
+
+class RasterFile(object):
+    def __init__(self, filename):
+        self.file = filename
+        self._extent = None
+        self._elevation = None
+
+    @property
+    def file(self):
+        return self._file
+
+    @file.setter
+    def file(self, filename):
+        self._file = gdal.Open(filename)
+
+    @property
+    def extent(self):
+        if self._extent is None:
+            gt = self.geo_transform()
+            x_min = gt[0]
+            x_max = gt[0] + self.file.RasterXSize / gt[1]
+            y_min = gt[3] + self.file.RasterYSize / gt[5]
+            y_max = gt[3]
+
+            self._extent = x_min, x_max, y_min, y_max
+        return self._extent
+
+    @staticmethod
+    def get_values_for_raster(raster, band_number=1):
+        band = raster.GetRasterBand(band_number)
+        values = np.ma.masked_values(
+            band.ReadAsArray(), band.GetNoDataValue() or 0., copy=False
+        )
+        del band
+        return values
+
+    def get_raster_attribute(self, attribute):
+        raster = gdal.DEMProcessing('', self.file, attribute, format='MEM')
+        raster_values = self.get_values_for_raster(raster)
+        del raster
+        return raster_values
+
+    @property
+    def elevation(self):
+        if self._elevation is None:
+            self._elevation = self.get_values_for_raster(self.file)
+        return self._elevation
+
+    @property
+    def hill_shade(self):
+        return self.get_raster_attribute('hillshade')
+
+    def geo_transform(self):
+        return self.file.GetGeoTransform()
+
+
+class RasterSource(object):
+    FILE_ENDING = 'trans.tif'
+
+    def __init__(self, folder):
+        self._folder = folder
+
+    def file_list(self):
+        return glob.glob(
+            os.path.join(self._folder, '') + '**/*' + self.FILE_ENDING,
+            recursive=True
+        )
+
+
+def ols(x, **kwargs):
+    fit = sm.OLS(y, x, missing='drop',).fit()
+    return fit.intercept
+
+
+if __name__ == '__main__':
+    files = RasterSource(
+        '/Volumes/warehouse/projects/UofU/geohackweek/galcier_data/khumbu_DEM_32m/'
+    ).file_list()
+
+    raster_list = warplib.memwarp_multi_fn(
+        files, extent='union', res='min', t_srs=files[0]
+    )
+
+    band_data = [iolib.ds_getma(i).filled(np.NaN) for i in raster_list]
+
+    x, y = geolib.get_xy_grids(raster_list[0])
+    time_list = np.array([timelib.fn_getdatetime(fn) for fn in files])
+
+    x_band_data = xr.DataArray(
+        np.stack(band_data),
+        coords={
+            'lat': y[::,0],
+            'lon': x[0,::],
+            'time': time_list
+        },
+        dims=('time', 'lat', 'lon'),
+        name='elevation'
+    )
+
+    x_band_data = x_band_data.sortby('time')
+
+    x_band_data['mean'] = x_band_data.mean(dim='time')
+    x_band_data['count'] = x_band_data.count(dim='time')
+    # shade = geolib.gdaldem_mem_ma(
+    #     x_band_data['mean'].values, ds=raster_list[0], returnma=True
+    # )
+    #
+    # extent = geolib.ds_geom_extent(raster_list[0])
+    #
+    # plt.imshow(shade, extent=extent, cmap='gray', clim=(1, 255))
+    # plt.imshow(x_band_data['mean'], alpha=0.4, extent=extent)
+    #
+    # plt.show()
+
+    # xr.apply_ufunc(ols,
+    #                x_band_data.groupby('time'),
+    #                input_core_dims=[['lat'], ['lon']],
+    #                output_dtypes=[float])
+
+    x_band_data.groupby('time').apply(ols)
